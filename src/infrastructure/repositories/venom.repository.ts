a521fcdcb673bd961e753d07538bdf452a470fbb
Roberto Vite <roberto.vite@vionico.mx>
2024-07-22 11:21:48 -0700

    Recent changes

--- src/infrastructure/repositories/venom.repository.ts
+++ src/infrastructure/repositories/venom.repository.ts
@@ -11,7 +11,9 @@ export class VenomTransporter implements LeadExternal {
   sendMsg(lead: { message: string; phone: string }): Promise<any> {
     try {
       const { message, phone } = lead;
-      const response = this.intance?.sendText(`${phone}@c.us`, message);
+      const postImage = "https://firebasestorage.googleapis.com/v0/b/mis-xvs.appspot.com/o/envioPaula.png?alt=media&token=99c3d94d-65e5-40ae-a6ea-59f32c198709";
+      const response = this.intance?.sendImage(`${phone}@c.us`,postImage,undefined,message);
+      // const response = this.intance?.sendText(`${phone}@c.us`, message);
       return Promise.resolve(response);
     } catch (error: any) {
       return Promise.reject(error);
